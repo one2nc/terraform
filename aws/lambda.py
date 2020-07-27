811f7a34a1f2f8de4adf20b9a53b5995607a0d32
shashikarsiddharth <siddharth.shashikar@trustingsocial.com>
2020-07-27 14:30:23 +0530

    added lambda function

---
+++ aws/lambda.py
@@ -0,0 +1,3 @@
+def lambda_handler(event, context):
+    print("Hello World from AWS Lambda!")
+    return "Hello World from AWS Lambda!"
---
+++ aws/lambda.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "lambda_func.zip"
+  function_name    = "lambda_handler"
+  handler          = "lambda.lambda_handler"
+  role             = aws_iam_role.iam_for_lambda.arn
+  source_code_hash = filebase64sha256("lambda_func.zip")
+  runtime          = "python3.8"
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+//  policy_arn = aws_iam_policy.lambda_logging.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_cloudwatch_log_group" "test_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.test_lambda.function_name}"
+  retention_in_days = 1
+}
