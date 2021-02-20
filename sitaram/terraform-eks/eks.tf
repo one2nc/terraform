4da0f13ae1320b00485f2090bc970baa274c9bdb
akshay-sri123 <akshay@one2n.in>
2021-02-20 11:46:53 +0530

    Add eks node pool resources

--- sitaram/terraform-eks/eks.tf
+++ sitaram/terraform-eks/eks.tf
@@ -6,25 +6,78 @@ output "cluster_name" {
   value = null_resource.eks_cluster.triggers.name
 }
 
+output "cluster_ca" {
+  value = aws_eks_cluster.eks_cluster.certificate_authority.0.data
+}
+
+output "kubernetes_version" {
+  value = aws_eks_cluster.eks_cluster.version
+}
+
 variable "eks_version" {
   type    = string
   default = "1.18"
+}
+
+variable "eks_node_group_size" {
+  type    = string
+  default = "t3.micro"
 }
 
 resource "null_resource" "eks_cluster" {
   triggers = {
     name = "${local.project_prefix}-cluster"
   }
+}
 
+resource "tls_private_key" "private" {
+  algorithm = "RSA"
+}
+
+resource "aws_key_pair" "private" {
+  key_name = "eks_private"
+  public_key = "${tls_private_key.private.public_key_openssh}"
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = null_resource.eks_cluster.triggers.name
-  role_arn = aws_iam_role.eks_iam.arn
+  role_arn = aws_iam_role.eks_cluster.arn
   version  = var.eks_version
   vpc_config {
     subnet_ids = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
   }
   tags       = null_resource.tags.triggers
-  depends_on = [aws_iam_role_policy_attachment.eks-iam-AmazonEKSClusterPolicy]
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster-AmazonEKSServicePolicy
+  ]
+}
+
+
+resource "aws_eks_node_group" "eks_cluster" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${null_resource.eks_cluster.triggers.name}-node-group"
+  node_role_arn   = aws_iam_role.eks_cluster_worker.arn
+  subnet_ids      = aws_subnet.private.*.id
+  instance_types = [var.eks_node_group_size]
+  
+  remote_access {
+    ec2_ssh_key = aws_key_pair.private.key_name
+  }
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 3
+    min_size     = 3
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks_cluster-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks_cluster-AmazonEC2ContainerRegistryReadOnly,
+  ]
+
+  tags = {
+    Name = "${local.project_prefix}-cluster"
+  }
 }
--- sitaram/terraform-eks/iam.tf
+++ sitaram/terraform-eks/iam.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "eks_iam" {
-  name = "${local.project_prefix}-eks-manager"
+resource "aws_iam_role" "eks_cluster" {
+  name = "${local.project_prefix}-eks-role"
 
   assume_role_policy = <<POLICY
 {
@@ -17,7 +17,46 @@ resource "aws_iam_role" "eks_iam" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "eks-iam-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_iam.name
+resource "aws_iam_role" "eks_cluster_worker" {
+  name = "${local.project_prefix}-node-group-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
+  POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_cluster_worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_cluster_worker.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_cluster_worker.name
+}
+
