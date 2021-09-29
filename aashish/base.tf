149885500224cf3d36e053e3e5e1b56da61d6df2
Aashish Nehete <ashnehete@gmail.com>
2021-09-29 15:57:38 +0530

    Added route tables

--- aashish/base.tf
+++ aashish/base.tf
@@ -3,8 +3,7 @@ variable "region" {
 }
 
 variable "cidr_block" {
-  type    = string
-  default = "10.10.0.0/16"
+  type = string
 }
 
 variable "project" {
@@ -30,6 +29,8 @@ locals {
   }
 
   no_of_az = length(data.aws_availability_zones.available.names)
+
+  default_route = "0.0.0.0/0"
 }
 
 data "aws_availability_zones" "available" {
@@ -50,8 +51,8 @@ resource "aws_subnet" "public" {
   count                   = local.no_of_az
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = cidrsubnet(var.cidr_block, 4, count.index)
-  map_public_ip_on_launch = false
+  cidr_block              = cidrsubnet(var.cidr_block, 3, count.index)
+  map_public_ip_on_launch = true
 
   tags = merge({
     Name = "${local.project_name}-public-subnet-${count.index}"
@@ -62,8 +63,8 @@ resource "aws_subnet" "private" {
   count                   = local.no_of_az
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = cidrsubnet(var.cidr_block, 4, local.no_of_az + count.index)
-  map_public_ip_on_launch = true
+  cidr_block              = cidrsubnet(var.cidr_block, 3, local.no_of_az + count.index)
+  map_public_ip_on_launch = false
 
   tags = merge({
     Name = "${local.project_name}-private-subnet-${count.index}"
@@ -101,4 +102,57 @@ resource "aws_nat_gateway" "nat" {
   tags = merge({
     Name = "${local.project_name}-nat-${count.index}"
   }, local.tags)
+}
+
+# Route Tables
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  depends_on = [
+    aws_vpc.main_vpc,
+    aws_internet_gateway.main_igw
+  ]
+
+  tags = merge({
+    Name = "${local.project_name}-rt-public"
+  }, local.tags)
+}
+
+resource "aws_route_table" "private" {
+  count  = local.no_of_az
+  vpc_id = aws_vpc.main_vpc.id
+
+  depends_on = [
+    aws_vpc.main_vpc,
+    aws_internet_gateway.main_igw
+  ]
+
+  tags = merge({
+    Name = "${local.project_name}-rt-private-${count.index}"
+  }, local.tags)
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = local.default_route
+  gateway_id             = aws_internet_gateway.main_igw.id
+}
+
+resource "aws_route" "private" {
+  count                  = local.no_of_az
+  route_table_id         = aws_route_table.private[count.index].id
+  destination_cidr_block = local.default_route
+  nat_gateway_id         = aws_nat_gateway.nat[count.index].id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = local.no_of_az
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = local.no_of_az
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
